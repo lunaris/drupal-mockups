ab526763aa73934cbd584aac4cfb09fd50d55b04
Will Jones <will@sacharissa.co.uk>
2013-02-10 20:49:00 +0000

    Make sure that Span/Column initialize and render correctly.

--- mockups/js/models/span.js
+++ mockups/js/models/span.js
@@ -5,6 +5,9 @@ define([
   'models/box'
 ], function(_, Backbone, Box) {
   var Span = Box.extend({
+    initialize: function() {
+      this.constructor.__super__.initialize(this, _.rest(arguments));
+    }
   });
 
   return Span;
--- mockups/js/views/column.js
+++ mockups/js/views/column.js
@@ -3,10 +3,22 @@ define([
   'underscore',
   'backbone',
 
+  'models/column',
+
   'views/box'
-], function($, _, Backbone, BoxView) {
+], function($, _, Backbone, Column, BoxView) {
   var ColumnView = BoxView.extend({
     className: 'column',
+
+    render: function() {
+      this.constructor.__super__.render.apply(this, _.rest(arguments));
+
+      this.$el.css({
+        padding: this.model.get('padding')
+      });
+
+      return this;
+    }
   });
 
   return ColumnView;
